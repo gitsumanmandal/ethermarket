fcb2d7fb4f8800f837ad6f66f8e1e0a9b1f84eb6
Suman Mandal <38155383+gitsumanmandal@users.noreply.github.com>
2018-06-20 01:51:24 +0530

    Add files via upload

---
+++ contracts/ChainList.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.18;
+
+contract ChainList {
+  // state variables
+  address seller;
+  string name;
+  string description;
+  uint256 price;
+
+  // events
+  event LogSellArticle(
+    address indexed _seller,
+    string _name,
+    uint256 _price
+  );
+
+  // sell an article
+  function sellArticle(string _name, string _description, uint256 _price) public {
+    seller = msg.sender;
+    name = _name;
+    description = _description;
+    price = _price;
+
+    LogSellArticle(seller, name, price);
+  }
+
+  // get an article
+  function getArticle() public view returns (
+    address _seller,
+    string _name,
+    string _description,
+    uint256 _price
+  ) {
+      return(seller, name, description, price);
+  }
+}
